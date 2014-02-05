d1f591546d6fc8ca8d3d538e105820d0ac1c9378
wahyuaw <wahyu.ftiuksw@yahoo.com>
2014-02-05 13:44:03 +0700

    Delete package.js

--- package.js
+++
@@ -1,9 +0,0 @@
-Package.describe({
-    summary: "Easily create charts with Highcharts."
-});
-
-Package.on_use(function(api) {
-    api.use('jquery', 'client');
-    api.add_files('lib/highcharts.js', 'client');
-    api.add_files('lib/exporting.src.js', 'client');
-});
